ba16d53857aba2e467ec61ab64049fff4379e1fe
drazpa <divercityrecords@gmail.com>
2025-10-14 19:17:06 -0700

    Updated Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -350,7 +350,6 @@ export default function Dashboard() {
       if (cachedTokens) {
         console.log('✅ Using cached token list');
         setTokens(cachedTokens);
-        setFilteredTokens(cachedTokens);
         return;
       }
 
