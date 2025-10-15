5e7640c41edc57acfa6be13d39d4e77622652a0d
drazpa <divercityrecords@gmail.com>
2025-10-14 19:33:33 -0700

    Updated index.html

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'xrpl-vendor': ['xrpl'],
+          'charts-vendor': ['lightweight-charts'],
+        }
+      }
+    }
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
 })
